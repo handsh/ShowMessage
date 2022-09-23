6755b71150afd7c486219c323a934b61eb7cc565
handsh <handsh521@gmail.com>
2022-09-23 23:15:31 +0800

    add

---
+++ src/ShowMessage.java
@@ -0,0 +1,16 @@
+
+public class ShowMessage {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("韩冬爽");
+		System.out.println("软件技术");
+		System.out.println("2101212");
+		System.out.println("软件二班");
+
+
+
+
+	}
+
+}
